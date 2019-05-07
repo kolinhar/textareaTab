a9f65a5233b81dafdd4f00c7d3d4625e965712fe
Rémy JUANES <rjuanes@cae.com>
2019-05-07 12:08:34 -0400

    implement unTab

--- js/script.js
+++ js/script.js
@@ -1,17 +1,29 @@
 const txtA = document.querySelector("#txtA");
 txtA.addEventListener("keydown", Tabify);
 
-function Tabify(e){
+function Tabify(e) {
     if (e.key === "Tab" || e.code === "Tab") {
         e.preventDefault();
 
         const texte = txtA.value;
         const cursorPos = e.target.selectionStart;
-        const texteFinal = texte.substr(0, cursorPos) + "\t" + texte.substr(cursorPos, texte.lenght);
 
-        txtA.value = texteFinal;
-        e.target.selectionEnd = cursorPos + 1;
+        if (e.shiftKey === true && (cursorPos - 1) > -1) {
+            const precChar = texte.substr(cursorPos - 1, 1)
 
-        return false;
+            if (precChar === "\t" || precChar === " ") {
+                //untab
+                const texteFinal = texte.substr(0, cursorPos - 1) + texte.substr(cursorPos, texte.lenght)
+                txtA.value = texteFinal;
+                e.target.selectionEnd = cursorPos - 1;
+            }
+        }
+        else {
+            //tab
+            const texteFinal = texte.substr(0, cursorPos) + "\t" + texte.substr(cursorPos, texte.lenght);
+
+            txtA.value = texteFinal;
+            e.target.selectionEnd = cursorPos + 1;
+        }
     }
 }
