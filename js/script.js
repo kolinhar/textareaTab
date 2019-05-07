9a486c5877e343f50183b0ebcb32bcb4bb484d85
Rémy JUANES <rjuanes@cae.com>
2019-05-07 11:14:52 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const txtA = document.querySelector("#txtA");
+txtA.addEventListener("keydown", function (e) {
+    if (e.key === "Tab" || e.code === "Tab") {
+        e.preventDefault();
+
+        const texte = txtA.value;
+        const cursorPos = e.target.selectionStart;
+        const texteFinal = texte.substr(0, cursorPos) + "\t" + texte.substr(cursorPos, texte.lenght);
+
+        txtA.value = texteFinal;
+        e.target.selectionEnd = cursorPos + 1;
+
+        return false;
+    }
+});
